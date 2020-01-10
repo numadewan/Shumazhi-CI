8c4c379fcc637652a9705853138c30c924b9f48a
PeterYama <35582343+PeterYama@users.noreply.github.com>
2020-01-10 11:38:25 +1100

    Project created using Code Igniter Framework, Landing Page Added

---
+++ application/views/pages/home.php
@@ -0,0 +1,65 @@
+	<!-- Main Page -->
+		<div class="container">
+			
+			<div class="row" style=" height:50px; padding: 50px; margin-top:100px;">
+			
+				<div class="col s6"  style="padding: 10px;">
+					<div class="container">
+					<div class="row" style="margin-top: 50px;"><h3>Title<h3></div>
+					<p>
+					Lorem ipsum dolor sit amet. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+					<p>
+					</div>
+				<div class="col s6" >
+				<div class="row">
+
+	<!-- If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php". -->
+					<form class="col s12" action="/action_page.php" style="padding: 50px;">
+						<div class="row">
+							<div class="input-field col s6">
+							<input placeholder="First Name" id="first_name" type="text" class="validate">
+							</div>
+							<div class="input-field col s6">
+							<input id="last_name"  placeholder="Last Name"type="text" class="validate">
+							</div>
+						</div>
+
+						
+						<div class="row">
+
+							<div class="input-field col s12">
+								<input placeholder="Company Name" id="first_name" type="text" class="validate">
+							</div>
+
+							<div class="input-field col s12">
+								<input id="email" type="email" class="validate">
+								<label for="email">Email</label>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="input-field col s12">
+								<input id="password" type="password" class="validate">
+								<label for="password">Password</label>
+							</div>
+							
+						</div>
+						<div class="input-field col s12">
+							<i class="material-icons prefix">phone</i>
+							<input id="icon_telephone" type="tel" class="validate">
+							<label for="icon_telephone">Phone</label>
+						</div>
+					</form>
+					
+					<select id="country" name="country">a</select>
+
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+</div>
+
---
+++ application/views/templates/footer.php
@@ -0,0 +1,9 @@
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+     <!--JavaScript at end of body for optimized loading-->
+	 <script type="text/javascript" src="index.js"></script>
+	</body>
+</html>
