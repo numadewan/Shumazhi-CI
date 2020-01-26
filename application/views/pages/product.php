9137edd21e652639f5494fca35b1543a83cc6c94
Sujata Maharjan <msuzy239@gmail.com>
2020-01-26 18:18:29 +1100

    Login page logic along with form validations

--- application/views/pages/product.php
+++ application/views/pages/product.php
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <div class="container" style="padding:10;">
 
-=======
 <div class="container">
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-=======
+
 <div class="container" style="padding:10;">
 
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
 	<div class="row" style="padding:10px;">
 		<div class="col s6" style="margin-top: 10px;">Product Page</div>
 		<form action="" class="browser-default right">
@@ -22,8 +17,6 @@
 	</div>
 	<div class="row">
 		<div class="col s12 ">
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div class="card hoverable" >
         <div class="card-content dark-text">
         <?php if ($this->session->flashdata()) { ?>
@@ -31,14 +24,12 @@
             <?= $this->session->flashdata('msg'); ?>
         </div>
     <?php } ?>
-=======
       <div class="card #388e3c green darken-2" >
         <div class="card-content white-text">
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-=======
+
       <div class="card hoverable" >
         <div class="card-content dark-text">
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
+
           <span class="card-title">Card Title</span>
           <p>I am a very simple card. I am good at containing small bits of information.</p>
         </div>
@@ -47,21 +38,19 @@
           <a href="#">This is a link</a>
         </div>
       </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         <!-- <Button class="btn" href="<?php echo base_url(); ?>/product_registration" style="float:right;">Add Product</Button> -->
         <a style="float: right;"class="btn-floating btn-large waves-effect waves-light light-green" href="<?php echo base_url(); ?>/product_registration"><i class="material-icons">add</i></a>
         <? echo 'hello'?>
     </div>
-=======
+
     </div>
 		<div class="col s12">Hello</div>
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-=======
+
         <!-- <Button class="btn" href="<?php echo base_url(); ?>/product_registration" style="float:right;">Add Product</Button> -->
         <a style="float: right;"class="btn-floating btn-large waves-effect waves-light light-green" href="<?php echo base_url(); ?>/product_registration"><i class="material-icons">add</i></a>
         <? echo 'hello'?>
     </div>
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
+
 	</div>
 <div>
