8615a0c16c7c61d072585d7b0453411bc63ce4fa
Sujata Maharjan <msuzy239@gmail.com>
2020-01-26 18:16:31 +1100

    Login page logic along with form validations

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,36 +1,24 @@
 	<!-- Main Page -->
 	<div class="container">
 	<!-- Status message -->
-<<<<<<< HEAD
-    <!-- <?php  
-=======
     <?php  
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
         if(!empty($success_msg)){ 
             echo '<p class="status-msg success">'.$success_msg.'</p>'; 
         }elseif(!empty($error_msg)){ 
             echo '<p class="status-msg error">'.$error_msg.'</p>'; 
         } 
-<<<<<<< HEAD
-    ?> -->
-=======
     ?>
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-		
-			
+
 			<div class="row" style=" height:50px; padding: 50px; margin-top:100px;">
 			
 				<div class="col s6"  style="padding: 10px;">
 					<div class="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 					<div class="row" style="margin-top: 50px;"><h3>Shumazhi<h3></div>
-=======
+
 					<div class="row" style="margin-top: 50px;"><h3>Title<h3></div>
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-=======
+
 					<div class="row" style="margin-top: 50px;"><h3>Shumazhi<h3></div>
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
 					<p>
 					Lorem ipsum dolor sit amet. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
 					commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -40,7 +28,6 @@
 				<div class="col s6" >
 				<div class="row">
 
-<<<<<<< HEAD
 <!-- show error messages if the form validation fails -->
 <?php if ($this->session->flashdata()) { ?>
         <div class="alert alert-danger">
@@ -50,9 +37,7 @@
     <?php } ?>
 
 	<!-- If you click the "Submit" button, the form-data will be sent to controller called "User" and it's function "register". -->
-=======
-	<!-- If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php". -->
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
+
 					<form class="col s12" action="<?php echo base_url(); ?>user/register" method="post" style="padding: 50px;">
 						<div class="row">
 							<div class="input-field col s6">
