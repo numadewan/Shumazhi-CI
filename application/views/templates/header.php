28211c65e44668a1558f3ee80f01f2ba6631c282
Sujata Maharjan <msuzy239@gmail.com>
2020-01-26 18:11:58 +1100

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,8 +8,6 @@
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
       <!--Let browser know website is optimized for mobile-->
-<<<<<<< HEAD
-<<<<<<< HEAD
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
 <body>
@@ -29,11 +27,8 @@
 						<li><a href="<?php echo base_url(); ?>login">Login</a></li>
 						<li><a href="<?php echo base_url(); ?>product">Products</a></li>
 						<li><a href="<?php echo base_url(); ?>product_registration">product registration</a></li>
-=======
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-=======
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
 	</head>
 <body>
 	<ul id="dropdown1" class="dropdown-content">
@@ -48,16 +43,12 @@
 			<div class="nav-wrapper #81c784 green lighten-2">
 				<a href="#!" class="brand-logo"></a>
 					<ul class="right hide-on-med-and-down">
-<<<<<<< HEAD
 						<li><a href="<?php echo base_url(); ?>/product">Products</a></li>
 						<li><a href="badges.html">About</a></li>
->>>>>>> 1446bb4bea392b0409eca587309c98d46cc91d75
-=======
 						<li><a href="<?php echo base_url(); ?>home">Home</a></li>
 						<li><a href="<?php echo base_url(); ?>login">Login</a></li>
 						<li><a href="<?php echo base_url(); ?>product">Products</a></li>
 						<li><a href="<?php echo base_url(); ?>product_registration">product registration</a></li>
->>>>>>> 9a2a34601cbb733db622836c500512136eecb2bb
 						<!-- Dropdown Trigger -->
 						<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
 					</ul>
